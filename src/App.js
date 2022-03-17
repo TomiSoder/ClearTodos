f6bb8da76b4166a5286426746aef8431a6e1c554
Tomi Söder <tomi.soder1@fi.abb.com>
2022-03-17 15:02:12 +0200

    Tehtävä valmis

--- src/App.js
+++ src/App.js
@@ -14,13 +14,19 @@ function App() {
 
   const inputChanged = (event) => {
     setTodo({...todo, [event.target.name]: event.target.value});
-  } 
+  }
+
+  const clearTodo = (event) => {
+    event.preventDefault();
+    setTodos([]);
+  }
 
   return (
     <div className="App">
       <input type="text" placeholder="Date" name="date" value={todo.date} onChange={inputChanged}/>
       <input type="text" placeholder="Description" name="desc" value={todo.desc} onChange={inputChanged}/>
       <button onClick={addTodo}>Add</button>
+      <button onClick={clearTodo}>Clear</button>
       <TodoTable todos={todos} />
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import {fireEvent, render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+test('add todo', () => {
+  render(<App />)
+
+  const desc = screen.getByPlaceholderText('Description');
+  fireEvent.change(desc, {target: { value: 'Tee tehtävät loppuun'} });
+
+  const date = screen.getByPlaceholderText('Date');
+  fireEvent.change(date, {target: { value: '09.03.2022' } })
+
+  const addButton = screen.getByText('Add');
+  fireEvent.click(addButton);
+
+  const tablecell = screen.getByText(/tee tehtävät loppuun/i);
+  expect(tablecell).toBeInTheDocument();
+
+  const clearButton = screen.getByText('Clear');
+  fireEvent.click(clearButton);
+
+  expect(tablecell).not.toBeInTheDocument();
+
+})
